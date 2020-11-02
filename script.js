26655224ad3ded2731722274fc35aa84fab3e261
Maciej Wojtasiak <maciekwojtasiak@gmail.com>
2020-11-02 14:44:21 +0100

    preloader

--- script.js
+++ script.js
@@ -55,3 +55,9 @@ window.onscroll = function () {
 
 
 
+const preload = document.querySelector('.preload');
+
+preload.classList.add('show-preloader');
+window.addEventListener('load', () => {
+    preload.classList.remove('show-preloader');
+});
